8a7f8b393bf7c599275a5e42eb4371ba16d886b7
Joey Romo <joeyjr95@gmail.com>
2019-08-28 12:23:24 -0400

    moved files around

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import $ from 'jquery';
+'use strict';
+import {api} from './scripts';
+console.log(api);
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
 
-function main() {
-  console.log('DOM is loaded');
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
 
-  const startMsg = $('<p>Webpack is working!</p>');
-  $('#root').append(startMsg);
-}
-
-$(main);
